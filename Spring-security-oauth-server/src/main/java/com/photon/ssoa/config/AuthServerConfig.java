57f164c09d9341cdcc014ca2e323112265914fe6
Manas Laha <manas080199@gmail.com>
2021-12-07 09:50:07 +0530

    Session-n

--- Spring-security-oauth-server/src/main/java/com/photon/ssoa/config/AuthServerConfig.java
+++ Spring-security-oauth-server/src/main/java/com/photon/ssoa/config/AuthServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -23,9 +24,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
         clients.inMemory().withClient("app1").secret("sec1").authorizedGrantTypes("password","refresh_token").scopes("webclient")
                 .accessTokenValiditySeconds(300)
-                .and().withClient("app2").secret("sec2").authorizedGrantTypes("authorization_code").scopes("webclient")
+                .and().withClient("app2").secret("sec2").authorizedGrantTypes("authorization_code","refresh_token").scopes("webclient")
                 .redirectUris("http://localhost:30005/home").accessTokenValiditySeconds(300)
-                .and().withClient("/")
+                .and().withClient("app3").secret("sec3").authorizedGrantTypes("client_credentials").scopes("webclient")
+                .and().withClient("app4").secret("sec4").authorizedGrantTypes("implicit").scopes("webclient").redirectUris("http://localhost:30006/home").accessTokenValiditySeconds(300)
         .and().withClient("resource").secret("secret");
     }
 
---
+++ spring-security-oauth-resourceserver/src/main/java/com/photon/ssor/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.photon.ssor.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+
+        return new ResponseEntity<>("Hello from oauth-resourceserver", HttpStatus.OK);
+    }
+}
